d8621401517a816dde89fb558fe743cce228ab47
Jeff Scott Ward <jsward.17@gmail.com>
2014-01-06 22:55:40 -0500

    base architecture in place

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,76 @@
   'use strict';
 
   $(function () {
-    // FireShell
+
+    var BMBAPP = function(){
+
+      this.data = {
+        currentScreen: 'undefined'  
+      };
+
+      this.dom = {
+        screen: {
+          collection: $('.screen')
+        }
+      };
+
+      this.screen = function(){
+
+        var thatData = this.data;
+        var thatDom = this.dom;
+
+        function getScreen(screenName) {
+          return thatDom.screen.collection.filter( '.' + screenName );
+        }
+
+        function getCurrentScreen() {
+          return thatData.currentScreen;
+        }
+
+        function updateScreenStatus(screenName) {
+          thatData.currentScreen = screenName;
+        }
+
+        function clearAllScreens() {
+          updateScreenStatus('undefined');
+          thatDom.screen.collection.removeClass('-active-screen');
+        }
+
+        function turnOffScreen(screenName) {
+          updateScreenStatus('undefined');
+          getScreen(screenName).removeClass('-active-screen');
+        }
+
+        function turnOnScreen(screenName) {
+          updateScreenStatus(screenName);
+          getScreen(screenName).addClass('-active-screen');
+        }
+        
+        function goToScreen(screenName){
+          clearAllScreens();
+          updateScreenStatus(screenName);
+          turnOnScreen(screenName);
+          console.log('Current Screen: ' + getCurrentScreen());
+        }
+
+        return {
+
+          getScreen: getScreen,
+          getCurrentScreen: getCurrentScreen,
+          updateScreenStatus: updateScreenStatus,
+          clearAllScreens: clearAllScreens,
+          turnOffScreen: turnOffScreen,
+          turnOnScreen: turnOnScreen,
+          goToScreen: goToScreen
+
+        };
+
+      };
+
+    };
+
+    window.appInstance = new BMBAPP();
+
   });
 
 })(jQuery, window, document);
--- src/js/sunburst.js
+++ src/js/sunburst.js
@@ -1,72 +1,72 @@
 'use strict';
 
-var Sunburst = (function() {
+// var Sunburst = (function() {
 	
-	var _drawTriangle = function(ctx, fatness, maxDimen) {
-		var xPos = (fatness/100) * maxDimen;
-		var xNeg = -1 * xPos;
-		ctx.beginPath();
-		ctx.moveTo(0, 0);
-		ctx.lineTo(maxDimen*Math.sqrt(2), xNeg);
-		ctx.lineTo(maxDimen*Math.sqrt(2), xPos);
-		ctx.fill();
-	};
+// 	var _drawTriangle = function(ctx, fatness, maxDimen) {
+// 		var xPos = (fatness/100) * maxDimen;
+// 		var xNeg = -1 * xPos;
+// 		ctx.beginPath();
+// 		ctx.moveTo(0, 0);
+// 		ctx.lineTo(maxDimen*Math.sqrt(2), xNeg);
+// 		ctx.lineTo(maxDimen*Math.sqrt(2), xPos);
+// 		ctx.fill();
+// 	};
 
-	var _sunburst = function(canvas, options) {
-		var $$ = function(val, deflt) {
-			if('undefined'===typeof val) { return deflt; }
-			return val;
-		};
+// 	var _sunburst = function(canvas, options) {
+// 		var $$ = function(val, deflt) {
+// 			if('undefined'===typeof val) { return deflt; }
+// 			return val;
+// 		};
 		
-		var ctx = canvas.getContext('2d');
-		ctx.save();
+// 		var ctx = canvas.getContext('2d');
+// 		ctx.save();
 		
-		options = $$(options, {});
-		var bars = $$(options.bars, 36);
-		var fatness = $$(options.fatness, 5);
-		var color = $$(options.color, 'black');
-		var backcolor = $$(options.backcolor, 'white');
+// 		options = $$(options, {});
+// 		var bars = $$(options.bars, 36);
+// 		var fatness = $$(options.fatness, 5);
+// 		var color = $$(options.color, 'black');
+// 		var backcolor = $$(options.backcolor, 'white');
 		
-		var width = canvas.width;
-		var height = canvas.height;
-		var maxDimen = (height > width ? height : width);
+// 		var width = canvas.width;
+// 		var height = canvas.height;
+// 		var maxDimen = (height > width ? height : width);
 		
-		ctx.translate(width/2,height/2);
-		ctx.fillStyle = backcolor;
-		ctx.fillRect(-1* (width/2),-1 * (height/2),width,height);
-		ctx.fillStyle = color;
-		for(var i = 0; i < bars; i++) {
-			ctx.save();
-			ctx.rotate(i*(Math.PI*2/bars));
-			_drawTriangle(ctx, fatness, maxDimen);
-			ctx.restore();
-		}
+// 		ctx.translate(width/2,height/2);
+// 		ctx.fillStyle = backcolor;
+// 		ctx.fillRect(-1* (width/2),-1 * (height/2),width,height);
+// 		ctx.fillStyle = color;
+// 		for(var i = 0; i < bars; i++) {
+// 			ctx.save();
+// 			ctx.rotate(i*(Math.PI*2/bars));
+// 			_drawTriangle(ctx, fatness, maxDimen);
+// 			ctx.restore();
+// 		}
 		
-		ctx.restore();
-	};
+// 		ctx.restore();
+// 	};
 		
-	return {Draw : _sunburst};
-})();
+// 	return {Draw : _sunburst};
+// })();
 
-var init = function(){
-	var canvas = document.getElementById('burst');
-	canvas.width = document.body.clientWidth * 2;
-	canvas.height = document.body.clientHeight * 3;
-	canvas.style.position = 'fixed';
-	canvas.style.left = -(document.body.clientWidth/2) + 'px';
-	canvas.style.top = -(document.body.clientHeight) + 'px';
+// var init = function(){
+// 	var canvas = document.getElementById('burst');
+// 	canvas.width = document.body.clientWidth * 2;
+// 	canvas.height = document.body.clientHeight * 3;
+// 	canvas.style.position = 'fixed';
+// 	canvas.style.left = -(document.body.clientWidth/2) + 'px';
+// 	canvas.style.top = -(document.body.clientHeight) + 'px';
 
-	Sunburst.Draw(canvas,{
-	  bars: 24,
-	  fatness: 10,
-	  color: 'rgba(253, 236, 0, 1.0)',
-	  backcolor: 'rgba(238, 114, 0, 1.0)'
-	});
-};
+// 	Sunburst.Draw(canvas,{
+// 	  bars: 24,
+// 	  fatness: 10,
+// 	  color: 'rgba(253, 236, 0, 1.0)',
+// 	  backcolor: 'rgba(238, 114, 0, 1.0)'
+// 	});
+// };
 
-init();
+// init();
 
-window.onresize = function(){
-	init();
-};
+// window.onresize = function(){
+// 	init();
+// };
 
